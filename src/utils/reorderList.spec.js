fc4dd1f7c883b7438caa56fc7b954436372e23b9
Dima Vyshniakov <morewings@gmail.com>
2019-12-28 15:35:24 +0100

    typo

--- src/utils/reorderList.spec.js
+++ src/utils/reorderList.spec.js
@@ -1,7 +1,7 @@
 import reorderList from './reorderList';
 
 describe('reorderList', () => {
-  const list = Object.freeze(['a', 'b', 'c', 'd', 'e']); // freexing mock to check if mutation happens
+  const list = Object.freeze(['a', 'b', 'c', 'd', 'e']); // freeze mock to check if mutation happens
   it('reorders array based on provided params', () => {
     expect(reorderList(0, 3, list)).toEqual(['b', 'c', 'd', 'a', 'e']);
   });
